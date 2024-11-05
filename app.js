34c62029f19abdea54711947ad8762fa1a35a7ca
Mehmood M.Ashraf <mehmoodfazlani@gmail.com>
2024-11-06 01:43:44 +0500

    UI Complete

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+
+function startGame(){
+    let welcomePage = document.querySelector("#welcomePage");
+    welcomePage.style.display = "none"
+    let startGame = document.querySelector("#startGame");
+    startGame.style.display = "flex"
+}
